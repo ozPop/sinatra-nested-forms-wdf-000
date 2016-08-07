805c0b8f57aa7372b5e3de26dc204b3219a454f0
ozPop <ozziepopovas@gmail.com>
2016-08-07 16:05:01 -0400

    populate models

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  @@all = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @type = attributes[:type]
+    @booty = attributes[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
